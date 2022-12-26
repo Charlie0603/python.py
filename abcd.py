d1c0717673c1f651839fdc98aa9f8549e84187a3
Charlie0603 <95859907+Charlie0603@users.noreply.github.com>
2022-12-26 20:20:11 +0800

    Update abcd.py

--- abcd.py
+++ abcd.py
@@ -1,8 +1,15 @@
+import streamlit to st
 import requests
 from bs4 import BeautifulSoup
+import re
 
-url = 'https://water.taiwanstat.com/'
-web = requests.get(url)                        # 取得網頁內容
-soup = BeautifulSoup(web.text, "html.parser")  # 轉換成標籤樹
-title = soup.title                             # 取得 title
-print(title) 
+from requests.api import patch
+url1 = 'https://invoice.etax.nat.gov.tw/'
+html1 = requests.get(url1)
+sp = BeautifulSoup(html1.text, 'lxml')
+all1 = sp.find("table", class_="etw-table-bgbox etw-tbig")
+
+tb = sp.find('span', class_='font-weight-bold etw-color-red')
+st.write("特別獎:", tb.text)
+t = sp.find_all('span', class_='font-weight-bold etw-color-red')[1]
+st.write("特獎:", t.text)
